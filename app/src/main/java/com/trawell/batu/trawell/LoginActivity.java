a2fa97705ffb4488e89ad7e787e716ea9ca4a3b8
Batuhan İşlek <islekrb@gmail.com>
2018-03-17 22:09:14 +0300

    Initial commit

---
+++ app/src/main/java/com/trawell/batu/trawell/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.trawell.batu.trawell;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/trawell/batu/trawell/MainActivity.java
@@ -0,0 +1,42 @@
+package com.trawell.batu.trawell;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button signUpWithMailButton,
+                   loginButton;
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        signUpWithMailButton = findViewById(R.id.signup_button);
+        loginButton = findViewById(R.id.login_button);
+
+        signUpWithMailButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent signupActivityIntent = new Intent(view.getContext(),SignUpActivity.class);
+                startActivity(signupActivityIntent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent loginActivityIntent = new Intent(view.getContext(),LoginActivity.class);
+                startActivity(loginActivityIntent);
+            }
+        });
+
+    }
+}
